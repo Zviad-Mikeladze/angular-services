30e39f9d71c480f9c5dfa4f503ca07834a406555
Zviad-Mikeladze <zviadipf@gmail.com>
2022-12-08 16:34:58 +0400

    Service into service

--- src/app/logging.service.ts
+++ src/app/logging.service.ts
@@ -1,3 +1,6 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
 export class LoggingService {
   logSatusChange(status: string) {
     console.log('Aserver status changed , new status : ' + status);
